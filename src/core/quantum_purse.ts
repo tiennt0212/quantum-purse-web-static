970164b818e06a0faac17314201f0894d8ba4153
tea2x <copperheart05@gmail.com>
2025-03-04 14:11:30 +0700

    change name from key-unlocker to key-vault

--- src/core/quantum_purse.ts
+++ src/core/quantum_purse.ts
@@ -23,7 +23,7 @@ import { wordlist } from "@scure/bip39/wordlists/english";
 import { slh_dsa_shake_128f } from "@noble/post-quantum/slh-dsa";
 import __wbg_init, {
   KeyUnlocker,
-} from "../../key-unlocker/pkg/key_unlocker";
+} from "../../key-vault/pkg/key_unlocker";
 
 const { ckbHash } = utils;
 
